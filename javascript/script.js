314ac6ca2de88deeabb51b5b6ba709e3f27f93b6
amineennaciri <spitlight.enn@gmail.com>
2022-12-16 13:06:13 +0100

    Built the logic that checks for  when the game is over!  the checkWin function  check for 3-in-a-row, 3-in-a-column,  3-in-a-diagonal and a tie.

--- javascript/script.js
+++ javascript/script.js
@@ -18,6 +18,7 @@ const gameBox = document.querySelectorAll('.case')
 const xInput = 'x'
 const oInput = 'o'
 let lastPlay;
+let playCount = 0;
 console.log(gameBox)
 
 for(let i = 0;i<=gameBox.length-1;i++){
@@ -27,15 +28,53 @@ for(let i = 0;i<=gameBox.length-1;i++){
 function tictactoe(e){
     console.log(e.srcElement)
     console.log(e.srcElement.firstChild)
-    console.log(e.srcElement.firstChild.innerText==='') // logic that keeps players from playing in spots that are already taken
+    console.log(e.srcElement.firstChild.innerText==='') 
+    // logic that keeps players from playing in spots that are already taken
+    // and also enable players to play on a "1 turn principle"
     if(lastPlay === undefined && e.srcElement.firstChild.innerText === ''){
         e.srcElement.firstChild.innerText = xInput
         lastPlay = xInput
+        playCount++;
     }else if(lastPlay === xInput && e.srcElement.firstChild.innerText === ''){
         e.srcElement.firstChild.innerText = oInput
-        lastPlay = oInput    
+        lastPlay = oInput
+        playCount++;    
     }else if(lastPlay === oInput && e.srcElement.firstChild.innerText === ''){
         e.srcElement.firstChild.innerText = xInput
-        lastPlay = xInput    
+        lastPlay = xInput
+        playCount++;    
+    }
+    if(playCount>=5){
+        checkWin();// call this function
+    }
+}
+
+function checkWin(){
+    if(gameBox[0].firstChild.innerText!=''&& gameBox[0].firstChild.innerText === gameBox[1].firstChild.innerText && 
+    gameBox[1].firstChild.innerText === gameBox[2].firstChild.innerText){
+        alert('you win')
+    } else if(gameBox[3].firstChild.innerText!=''&& gameBox[3].firstChild.innerText === gameBox[4].firstChild.innerText && 
+    gameBox[4].firstChild.innerText === gameBox[5].firstChild.innerText){
+        alert('you win')
+    } else if(gameBox[6].firstChild.innerText!=''&& gameBox[6].firstChild.innerText === gameBox[7].firstChild.innerText && 
+    gameBox[7].firstChild.innerText === gameBox[8].firstChild.innerText){
+        alert('you win')
+    } else if(gameBox[0].firstChild.innerText!=''&& gameBox[0].firstChild.innerText === gameBox[3].firstChild.innerText && 
+    gameBox[3].firstChild.innerText === gameBox[6].firstChild.innerText){
+        alert('you win')
+    } else if(gameBox[1].firstChild.innerText!=''&& gameBox[1].firstChild.innerText === gameBox[4].firstChild.innerText && 
+    gameBox[4].firstChild.innerText === gameBox[7].firstChild.innerText){
+        alert('you win')
+    } else if(gameBox[2].firstChild.innerText!=''&& gameBox[2].firstChild.innerText === gameBox[5].firstChild.innerText && 
+    gameBox[5].firstChild.innerText === gameBox[8].firstChild.innerText){
+        alert('you win')
+    } else if(gameBox[0].firstChild.innerText!=''&& gameBox[0].firstChild.innerText === gameBox[4].firstChild.innerText && 
+    gameBox[4].firstChild.innerText === gameBox[8].firstChild.innerText){
+        alert('you win')
+    } else if(gameBox[6].firstChild.innerText!=''&& gameBox[6].firstChild.innerText === gameBox[4].firstChild.innerText && 
+    gameBox[4].firstChild.innerText === gameBox[2].firstChild.innerText){
+        alert('you win')
+    }else if(playCount===9){
+        alert(`it's a tie!`)
     }
 }
