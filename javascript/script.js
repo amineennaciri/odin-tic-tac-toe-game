4ec035c4ad91b48293568bfae1dd94bb64fc97bf
amineennaciri <spitlight.enn@gmail.com>
2022-12-16 18:18:44 +0100

    added a functionality so that once their is a winner the gameboard is locked and no player can add a mark. also the button which indicate the playerID will point to the user an indication telling them to restart the game to play one more time. this is great for user experience. I added a feature that flip the player order of turn so that if the first player start the 1st game, he won't start the next one, and vice versa for the second player. therefore achieving more equitability for both players.

--- javascript/script.js
+++ javascript/script.js
@@ -24,6 +24,7 @@ const oInput = 'o'
 let playerOne;
 let playerTwo;
 let lastPlay;
+let firstPlay;
 let playCount = 0;
 console.log(gameBox)
 
@@ -34,15 +35,24 @@ for(let i = 0;i<=gameBox.length-1;i++){
 btnStart.addEventListener('click', start)
 btnRestart.addEventListener('click', restart)
 
+// this is the function that render the content of the gameboard.
 function tictactoe(e){
+    if(scoreBoard.innerText!=''){
+        return ''
+    }
     console.log(e.srcElement)
     console.log(e.srcElement.firstChild)
     console.log(e.srcElement.firstChild.innerText==='') 
     // logic that keeps players from playing in spots that are already taken
     // and also enable players to play on a "1 turn principle"
     if(lastPlay === undefined && e.srcElement.firstChild.innerText === ''){
-        e.srcElement.firstChild.innerText = xInput
-        lastPlay = xInput
+        if(firstPlay === playerOne){
+            e.srcElement.firstChild.innerText = xInput
+            lastPlay = xInput
+        }else if(firstPlay === playerTwo){
+            e.srcElement.firstChild.innerText = oInput
+            lastPlay = oInput
+        }
         playCount++;
     }else if(lastPlay === xInput && e.srcElement.firstChild.innerText === ''){
         e.srcElement.firstChild.innerText = oInput
@@ -56,42 +66,68 @@ function tictactoe(e){
     if(playCount>=5){
         checkWin();// call this function
     }
-    // which players turn?
-    if(playCount%2==0){
-        btnPlayerId.innerText = `It's ${playerOne}'s turn now!`;
-    }else if(playCount%2!=0){
-        btnPlayerId.innerText = `It's ${playerTwo}'s turn now!`
+    // which players turn? the order flip vice versa depending on who played first
+    if(firstPlay === playerOne){
+        if(playCount%2==0 && scoreBoard.innerText === ''){
+            btnPlayerId.innerText = `It's ${playerOne}'s turn now!`;
+        }else if(playCount%2!=0 && scoreBoard.innerText === ''){
+            btnPlayerId.innerText = `It's ${playerTwo}'s turn now!`
+        }
+    }else if(firstPlay === playerTwo){
+        if(playCount%2!=0 && scoreBoard.innerText === ''){
+            btnPlayerId.innerText = `It's ${playerOne}'s turn now!`;
+        }else if(playCount%2==0 && scoreBoard.innerText === ''){
+            btnPlayerId.innerText = `It's ${playerTwo}'s turn now!`
+        }
     }
 }
-
+// this is the function that check for for when the game is over!
 function checkWin(){
     if(gameBox[0].firstChild.innerText!=''&& gameBox[0].firstChild.innerText === gameBox[1].firstChild.innerText && 
     gameBox[1].firstChild.innerText === gameBox[2].firstChild.innerText){
         //alert('you win')
         //scoreBoard.innerText = 'You Win !'
+        btnPlayerId.innerText = 'Click on restart to play again!'
+        btnPlayerId.style.backgroundColor = 'red'
         return gameBox[0].firstChild.innerText===xInput? scoreBoard.innerText = `${playerOne} Win !` : scoreBoard.innerText = `${playerTwo} Win !`
     } else if(gameBox[3].firstChild.innerText!=''&& gameBox[3].firstChild.innerText === gameBox[4].firstChild.innerText && 
     gameBox[4].firstChild.innerText === gameBox[5].firstChild.innerText){
+        btnPlayerId.innerText = 'Click on restart to play again!'
+        btnPlayerId.style.backgroundColor = 'red'
         return gameBox[0].firstChild.innerText===xInput? scoreBoard.innerText = `${playerOne} Win !` : scoreBoard.innerText = `${playerTwo} Win !`
     } else if(gameBox[6].firstChild.innerText!=''&& gameBox[6].firstChild.innerText === gameBox[7].firstChild.innerText && 
     gameBox[7].firstChild.innerText === gameBox[8].firstChild.innerText){
+        btnPlayerId.innerText = 'Click on restart to play again!'
+        btnPlayerId.style.backgroundColor = 'red'
         return gameBox[0].firstChild.innerText===xInput? scoreBoard.innerText = `${playerOne} Win !` : scoreBoard.innerText = `${playerTwo} Win !`
     } else if(gameBox[0].firstChild.innerText!=''&& gameBox[0].firstChild.innerText === gameBox[3].firstChild.innerText && 
     gameBox[3].firstChild.innerText === gameBox[6].firstChild.innerText){
+        btnPlayerId.innerText = 'Click on restart to play again!'
+        btnPlayerId.style.backgroundColor = 'red'
         return gameBox[0].firstChild.innerText===xInput? scoreBoard.innerText = `${playerOne} Win !` : scoreBoard.innerText = `${playerTwo} Win !`
     } else if(gameBox[1].firstChild.innerText!=''&& gameBox[1].firstChild.innerText === gameBox[4].firstChild.innerText && 
     gameBox[4].firstChild.innerText === gameBox[7].firstChild.innerText){
+        btnPlayerId.innerText = 'Click on restart to play again!'
+        btnPlayerId.style.backgroundColor = 'red'
         return gameBox[0].firstChild.innerText===xInput? scoreBoard.innerText = `${playerOne} Win !` : scoreBoard.innerText = `${playerTwo} Win !`
     } else if(gameBox[2].firstChild.innerText!=''&& gameBox[2].firstChild.innerText === gameBox[5].firstChild.innerText && 
     gameBox[5].firstChild.innerText === gameBox[8].firstChild.innerText){
+        btnPlayerId.innerText = 'Click on restart to play again!'
+        btnPlayerId.style.backgroundColor = 'red'
         return gameBox[0].firstChild.innerText===xInput? scoreBoard.innerText = `${playerOne} Win !` : scoreBoard.innerText = `${playerTwo} Win !`
     } else if(gameBox[0].firstChild.innerText!=''&& gameBox[0].firstChild.innerText === gameBox[4].firstChild.innerText && 
     gameBox[4].firstChild.innerText === gameBox[8].firstChild.innerText){
+        btnPlayerId.innerText = 'Click on restart to play again!'
+        btnPlayerId.style.backgroundColor = 'red'
         return gameBox[0].firstChild.innerText===xInput? scoreBoard.innerText = `${playerOne} Win !` : scoreBoard.innerText = `${playerTwo} Win !`
     } else if(gameBox[6].firstChild.innerText!=''&& gameBox[6].firstChild.innerText === gameBox[4].firstChild.innerText && 
     gameBox[4].firstChild.innerText === gameBox[2].firstChild.innerText){
+        btnPlayerId.innerText = 'Click on restart to play again!'
+        btnPlayerId.style.backgroundColor = 'red'
         return gameBox[0].firstChild.innerText===xInput? scoreBoard.innerText = `${playerOne} Win !` : scoreBoard.innerText = `${playerTwo} Win !`
     }else if(playCount===9){
+        btnPlayerId.innerText = 'Click on restart to play again!'
+        btnPlayerId.style.backgroundColor = 'red'
         scoreBoard.innerText = `It's a Tie!`
     }
 }
@@ -102,6 +138,7 @@ function start(){
     document.querySelector('.main-content').style.visibility = 'visible'
     document.querySelector('.score-board').style.visibility = 'visible'
     btnPlayerId.innerText = `It's ${playerOne}'s turn now!`
+    firstPlay = playerOne;
 }
 
 function restart(){
@@ -112,4 +149,13 @@ function restart(){
     scoreBoard.innerText = "";
     lastPlay = undefined;
     playCount = 0;
+    btnPlayerId.style.backgroundColor = 'black'
+    if(firstPlay === playerOne){
+        btnPlayerId.innerText = `It's ${playerTwo}'s turn now!`
+        firstPlay = playerTwo
+    }else if(firstPlay === playerTwo){
+        btnPlayerId.innerText = `It's ${playerOne}'s turn now!`
+        firstPlay = playerOne
+    }
+    
 }
