0b6572d64ddbd10b9e51676a6eee2c598ae90a0b
amineennaciri <spitlight.enn@gmail.com>
2022-12-16 12:28:37 +0100

    Built the function tictactoe() that allow players to add marks to a specific spot on the board, and then tied it to the DOM, letting players click on the gameboard to place their marker. Added a logic that keeps players from playing in spots that are already taken!

--- javascript/script.js
+++ javascript/script.js
@@ -1,3 +1,4 @@
+/* previous work
 let gameboard = {}// create object
 // properties
 gameboard.data = []
@@ -12,4 +13,29 @@ function GameboardGame(playerName){
         console.log('beep')
     }
 }
+*/
+const gameBox = document.querySelectorAll('.case')
+const xInput = 'x'
+const oInput = 'o'
+let lastPlay;
+console.log(gameBox)
 
+for(let i = 0;i<=gameBox.length-1;i++){
+    gameBox[i].addEventListener('click',tictactoe)
+}
+
+function tictactoe(e){
+    console.log(e.srcElement)
+    console.log(e.srcElement.firstChild)
+    console.log(e.srcElement.firstChild.innerText==='') // logic that keeps players from playing in spots that are already taken
+    if(lastPlay === undefined && e.srcElement.firstChild.innerText === ''){
+        e.srcElement.firstChild.innerText = xInput
+        lastPlay = xInput
+    }else if(lastPlay === xInput && e.srcElement.firstChild.innerText === ''){
+        e.srcElement.firstChild.innerText = oInput
+        lastPlay = oInput    
+    }else if(lastPlay === oInput && e.srcElement.firstChild.innerText === ''){
+        e.srcElement.firstChild.innerText = xInput
+        lastPlay = xInput    
+    }
+}
